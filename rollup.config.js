f8d4725fbde4c1fa661fd9d1ada98ea8bb7bc386
Clear <576839360@qq.com>
2016-06-30 15:59:26 +0800

    add configure file

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+export const paths = {
+    root: '/',
+    source: {
+        root: './src/',
+        modules: './src/modules/',
+    },
+    dist: {
+        root: './dist/',
+    },
+};
+
+export default {
+	moduleName: 'sharify', // var sharify = (function () { ...
+	moduleId: 'sharify', // define(['sharify'], ...
+}
